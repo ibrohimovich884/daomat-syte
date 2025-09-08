548cbc77358c083c047eae14c413db148ba8c5a8
Oybek Coder <bekmirzayevoybek575@gmail.com>
2025-09-08 17:00:28 +0500

    Add files via upload

---
+++ frontent/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>weddewr</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import Users from "./Users.model.js";
+import { createServer } from "http";
+
+const app = express();
+const httpServer = createServer(app);
+
+const con = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/social");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await Users.find();
+    console.log(users);
+    
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+con()
+
+httpServer.listen(3000, () => {
+  console.log(" Server running on http://localhost:3000");
+});
